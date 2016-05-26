138b3aa36f19fe8862f165fc9d938e66568d7394
aiguanghua <aiguanghua@kokozu.net>
2016-05-26 17:48:17 +0800

    Initial Commit

---
+++ RuntimeModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeModel
+//
+//  Created by 艾广华 on 16/5/26.
+//  Copyright © 2016年 艾广华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeModel
+//
+//  Created by 艾广华 on 16/5/26.
+//  Copyright © 2016年 艾广华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
