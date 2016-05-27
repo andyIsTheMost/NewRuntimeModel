d339ec905038e5f60ea63968ecf5546ac2782c7e
aiguanghua <aiguanghua@kokozu.net>
2016-05-27 10:25:48 +0800

    first commit

---
+++ RuntimeModel/ModelClass.m
@@ -0,0 +1,13 @@
+//
+//  ModelClass.m
+//  RuntimeModel
+//
+//  Created by 艾广华 on 16/5/26.
+//  Copyright © 2016年 艾广华. All rights reserved.
+//
+
+#import "ModelClass.h"
+
+@implementation ModelClass
+
+@end
---
+++ RuntimeModel/NSObject+KeyValues.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+KeyValues.m
+//  RuntimeModel
+//
+//  Created by 艾广华 on 16/5/26.
+//  Copyright © 2016年 艾广华. All rights reserved.
+//
+
+#import "NSObject+KeyValues.h"
+
+@implementation NSObject (KeyValues)
+
+@end
